907a373c6d038d245512e48e7d7b5491664650f9
Sidstumple <cydstumpel@msn.com>
2018-05-29 16:13:56 +0200

    start

---
+++ nuxt.config.js
@@ -0,0 +1,56 @@
+module.exports = {
+	/*
+	** Headers of the page
+	*/
+	head: {
+		title: 'Portfolio Cyd Stumpel',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: 'Portfolio Cyd Stumpel 2018' }
+		],
+		link: [
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+			{rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=PT+Mono'}
+		]
+	},
+	/*
+	** Global CSS
+	*/
+	css: [{
+		src: '~assets/scss/app.scss',
+		lang: 'scss'
+	}],
+	/*
+	** Customize the progress bar color
+	*/
+	loading: { color: '#3B8070' },
+	/*
+	** Build configuration
+	*/
+	build: {
+		postcss: {
+			plugins: {
+				'postcss-cssnext': {
+					browsers: ['last 2 versions', 'ie >= 9'],
+					features: {
+						customProperties: false
+					}
+				},
+			}
+		},
+		/*
+		** Run ESLint on save
+		*/
+		extend (config, { isDev, isClient }) {
+			if (isDev && isClient) {
+				config.module.rules.push({
+					enforce: 'pre',
+					test: /\.(js|vue)$/,
+					loader: 'eslint-loader',
+					exclude: /(node_modules)/
+				})
+			}
+		}
+	}
+}
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<section class="background-white">
+		<section class="circle"></section>
+		<div class="filter">
+			<img src="~/assets/img/cydBW.png" alt="">
+		</div>
+		<div class="intro">
+			<h1>My name is Cyd,</h1>
+			<p>I’m a web developer & designer based in Amsterdam. I studied Communication & Multimedia Design at the University of Applied Sciences.</p>
+			<p>Want to work together?</p>
+		</div>
+		<aside class="sidebar">
+			<p><span>Cyd Stumpel</span> — Web developer & designer — Amsterdam</p>
+		</aside>
+	</section>
+</template>
+
+<script>
+
+
+export default {
+	components: {
+
+	}
+};
+</script>
