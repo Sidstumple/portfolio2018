918e91e5e5b71832b3f324a81d17d91c426ec8d9
Sidstumple <cydstumpel@msn.com>
2018-06-21 13:10:54 +0200

    SVG genius

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,18 @@
 <template>
-	<section class="background-white">
-		<section class="circle"></section>
+	<section class="container">
 		<div class="filter">
-			<img src="~/assets/img/cydBW.png" alt="">
+			<svg xmlns="http://www.w3.org/2000/svg" version="1.1" height="0">
+				<filter id="duotone">
+					<feComponentTransfer color-interpolation-filters="sRGB" result="duotone">
+						<feFuncR type="table" tableValues="0.462745098 0.8196078431"></feFuncR>
+						<feFuncG type="table" tableValues="0.4509803922 0.9137254902"></feFuncG>
+						<feFuncB type="table" tableValues="0.6901960784 0.9215686275"></feFuncB>
+						<feFuncA type="table" tableValues="0 1"></feFuncA>
+					</feComponentTransfer>
+				</filter>
+				<circle cx="90vw" cy="100vh" r="80vw" fill="#D1E9EB" />
+				<image x="60vw" y="0" filter="url(#duotone)" width="100vw" height="100vh" xlink:href="~/assets/img/cydBW.png"/>
+			</svg>
 		</div>
 		<div class="intro">
 			<h1>My name is Cyd,</h1>
